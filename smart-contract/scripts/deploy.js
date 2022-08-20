a100c60a5687b27dab71eb956ec04d977d7e0a35
Punith Nandiraj <punithnandiraj@gmail.com>
2022-08-20 20:55:03 +0530

    Used Gun for chat and finished swap functionality

--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -1,31 +1,31 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const main = async () => {
+  const dogeFactory = await hre.ethers.getContractFactory('DogeCoin')
+  const dogeContract = await dogeFactory.deploy()
+  await dogeContract.deployed()
+  console.log('Dogecoin deployed to:', dogeContract.address)
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const linkFactory = await hre.ethers.getContractFactory('Link')
+  const linkContract = await linkFactory.deploy()
+  await linkContract.deployed()
+  console.log('LinkToken deployed to:', linkContract.address)
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const daiFactory = await hre.ethers.getContractFactory('Dai')
+  const daiContract = await daiFactory.deploy()
+  await daiContract.deployed()
+  console.log('DaiToken deployed to:', daiContract.address)
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  const usdcFactory = await hre.ethers.getContractFactory('Usdc')
+  const usdcContract = await usdcFactory.deploy()
+  await usdcContract.deployed()
+  console.log('UsdcToken deployed to:', usdcContract.address)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+;(async () => {
+  try {
+    await main()
+    process.exit(0)
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+})()
